ffaa6d843181c52b3571b0c7bbc1b0dc580b9bdf
Joel Jesudason <62968241+JoelJesudason@users.noreply.github.com>
2021-04-27 12:09:21 -0400

    Create testMaker.sol

---
+++ testMaker.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.8.0;
+
+import {
+    
+    ERC20
+} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract testMaker is ERC20 {
+    
+    address payable owner;
+    
+    modifier onlyOwnerCanMint {
+        require(msg.sender == owner, "You're not the owner. Minting denied");
+        _;
+    }
+    
+    constructor(uint initialSupply) ERC20("testMaker","MKRt") public {
+        owner = payable(msg.sender);
+        mint(owner,initialSupply);
+    }
+    
+    function mint(address recipient, uint amount) public onlyOwnerCanMint {
+        ERC20._mint(recipient,amount);
+    }
+    
+    // 000000000000000000 (18 zeros)
+    // 000,000,000
+    
+}
